a3276dc50dfc20e13e857951270b28927062bb5e
decagon <anietieudoaka@gmail.com>
2021-10-07 10:55:54 +0100

    Added documentation to entity mapper class

--- app/src/main/java/com/decagon/anietie/simplecrypto/util/EntityMapper.kt
+++ app/src/main/java/com/decagon/anietie/simplecrypto/util/EntityMapper.kt
@@ -1,5 +1,8 @@
 package com.decagon.anietie.simplecrypto.util
 
+/**
+ * An interface to map network models into domain models
+* */
 interface EntityMapper<Entity, Domain> {
 
     fun mapFromEntity(entity: Entity): Domain
